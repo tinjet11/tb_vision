8241c76f7ef3f7dce87b83178d72e850e34f78fe
tinjet <tinjet1016@gmail.com>
2024-11-05 20:03:16 +0800

    feat: define route

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tb_vision/src/auth/check_session.dart';
+import 'package:tb_vision/src/auth/login.dart';
+import 'package:tb_vision/src/auth/otp.dart';
 import 'package:tb_vision/src/home.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,11 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Camera App',
+      title: 'TB Vision',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  const Home() //const Login(),
+      //home:  const LoginPage(), //const Home(),
+      routes: {
+        "/": (context) => const CheckSession(), 
+        "/home": (context)=> const Home(),
+        "/login": (context) => const LoginPage(),
+        "/otp": (context)=> const OTPPage(),
+      },
     );
   }
 }
