4ce93f7554f468984543da6ef3e8a3e711bbf44e
tinjet <tinjet1016@gmail.com>
2025-03-10 22:01:04 +0800

    feat: implemented scrolling down reload page feature

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -22,38 +22,36 @@ class _HomeState extends State<Home> {
   Hospital? hospitalData;
   Questionnaire? questionnaireData;
   bool isLoading = true;
-  late bool isAnswerEmpty;
+
   @override
   void initState() {
     super.initState();
-
-    // Initialize DatabaseService
     dbService = DatabaseService(client, collections);
-    // Fetch data asynchronously
     fetchAdministrationData();
   }
 
   Future<void> fetchAdministrationData() async {
+    setState(() {
+      isLoading = true;
+    });
+
     try {
-      // Retrieve user ID
       var user = await getUser();
-      // Fetch patient document based on user ID
       var result = await dbService.listDocuments(
         'Administration',
-        [Query.equal('patient_id', user?.$id)],
+        [Query.equal('patient', user?.$id)],
       );
 
       if (result.documents.isNotEmpty) {
-        var adminDoc = result.documents.first;
+        var patientDoc = result.documents.first;
         setState(() {
-          administrationData = Administration.fromJson(adminDoc.data);
+          administrationData = Administration.fromJson(patientDoc.data);
           questionnaireData = administrationData?.questionnaireData;
           hospitalData = administrationData?.hospitalData;
         });
       }
     } catch (e) {
-      //print("Error fetching patient data: $e");
-      rethrow;
+      logger.e("Error fetching patient data: $e");
     } finally {
       setState(() {
         isLoading = false;
@@ -64,106 +62,92 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // App title section
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "TB Vision",
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
+      appBar: AppBar(toolbarHeight: 0),
+      body: RefreshIndicator(
+        onRefresh: fetchAdministrationData, // Pull-to-refresh
+        child: SingleChildScrollView(
+          physics:
+              const AlwaysScrollableScrollPhysics(), // Allows scroll even if content is short
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "TB Vision",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  const SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      logoutUser();
-                      Navigator.pushReplacementNamed(context, "/login");
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        logoutUser();
+                        Navigator.pushReplacementNamed(context, "/login");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
+                      child: const Text("Logout"),
                     ),
-                    child: const Text("Logout"),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            ProgressBar(
-              isLoading: isLoading,
-              administrationData: administrationData,
-            ),
-            // Upcoming Appointment section
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Upcoming Appointment",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+              ProgressBar(
+                  isLoading: isLoading, administrationData: administrationData),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                child: Text(
+                  "Upcoming Appointment",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            AppointmentCard(
-              administrationData: administrationData,
-              hospitalData: hospitalData,
-              isLoading: isLoading,
-            ),
-            const SizedBox(height: 16),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Action Item",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    "Patient action required. Please complete within the specified deadline.",
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey.shade600,
-                    ),
-                  ),
-                ],
+              AppointmentCard(
+                administrationData: administrationData,
+                hospitalData: hospitalData,
+                isLoading: isLoading,
               ),
-            ),
-            QuestionnaireCard(
-              administrationData: administrationData,
-              questionnaireData: questionnaireData,
-              isLoading: isLoading,
-            ),
-            AssessmentCard(
-              administrationData: administrationData,
-              isLoading: isLoading,
-            ),
-          ],
+              const SizedBox(height: 16),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Action Item",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      "Patient action required. Please complete within the specified deadline.",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              ),
+              QuestionnaireCard(
+                administrationData: administrationData,
+                questionnaireData: questionnaireData,
+                isLoading: isLoading,
+              ),
+              AssessmentCard(
+                administrationData: administrationData,
+                isLoading: isLoading,
+              ),
+            ],
+          ),
         ),
       ),
     );
